6a3541e2f372da9843f7a15dd3f6dfd95b75d043
sigboe <sig.boe@gmail.com>
2024-09-26 12:28:01 +0200

    Host: zig-pc-01, Generation: 270, Version: "24.05.20240920.944b2ae", Kernel: "6.6.52"

--- hosts/common/core/activationScripts.nix
+++ hosts/common/core/activationScripts.nix
@@ -1,18 +1,20 @@
-{ pkgs, self, ... }: {
+{ pkgs, self, config, ... }: {
   system.activationScripts = {
     gitcommit.text = ''
       etcNixos="$(readlink -f /etc/nixos)"
+      owner="$(stat -c '%U' $etcNixos)"
+      hostName="${config.networking.hostName}"
       internalOutPath="${self.sourceInfo.outPath}"
-      etcNixosOutPath="$(${pkgs.nix}/bin/nix flake metadata --json "$etcNixos" | ${pkgs.jq}/bin/jq -r .path)"
+      PATH="/run/current-system/sw/bin" etcNixosOutPath="$(${pkgs.nix}/bin/nix flake metadata --json "$etcNixos" | ${pkgs.jq}/bin/jq -r .path)"
 
-      if [ $internalOutPath == $etcNixosOutPath ] && \
-      ${pkgs.git}/bin/git -C $etcNixos rev-parse --is-inside-work-tree
+      if [ "$internalOutPath" == "$etcNixosOutPath" ] && \
+      sudo -u $owner ${pkgs.git}/bin/git -C "$etcNixos" rev-parse --is-inside-work-tree
       then
         metadata="$(${pkgs.nixos-rebuild}/bin/nixos-rebuild list-generations --json)"
-        gen="$(${pkgs.jq} '.[0]|.generation' <<< "$metadata")"
-        version="$(${pkgs.jq} '.[0]|.nixosVersion' <<< "$metadata")"
-        kernel="$(${pkgs.jq} '.[0]|.kernelVersion' <<< "$metadata")"
-        ${pkgs.git}/bin/git commit -am "Host: $HOST, Generation: $gen, Version: $version, Kernel: $kernel"
+        gen="$(${pkgs.jq}/bin/jq '.[0]|.generation' <<< "$metadata")"
+        version="$(${pkgs.jq}/bin/jq '.[0]|.nixosVersion' <<< "$metadata")"
+        kernel="$(${pkgs.jq}/bin/jq '.[0]|.kernelVersion' <<< "$metadata")"
+        sudo -u $owner ${pkgs.git}/bin/git -C "$etcNixos" commit -am "Host: $hostName, Generation: $gen, Version: $version, Kernel: $kernel"
       fi
     '';
   };
--- hosts/common/optional/sway.nix
+++ hosts/common/optional/sway.nix
@@ -1,50 +1,49 @@
-{
-  inputs,
-  pkgs,
-  ...
+{ inputs
+, pkgs
+, ...
 }: {
-  programs.sway = {
-    enable = true;
-    wrapperFeatures.gtk = true;
-    extraPackages = with pkgs; [
-      autotiling
-      bemenu
-      bemoji
-      inputs.self.outputs.packages.x86_64-linux.bitwarden-rofi
-      inputs.self.outputs.packages.x86_64-linux.bose-connect-app-linux
-      blueberry
-      brightnessctl
-      dmenu
-      dunst
-      foot
-      grim
-      j4-dmenu-desktop
-      kitty
-      libnotify
-      maim
-      pinentry-bemenu
-      slurp
-      sway-audio-idle-inhibit
-      swayidle
-      swaylock
-      wl-clipboard
-      wtype
-      xdg-desktop-portal-wlr
-      xwaylandvideobridge
-    ];
+  programs = {
+    sway = {
+      enable = true;
+      wrapperFeatures.gtk = true;
+      extraPackages = with pkgs; [
+        autotiling
+        bemenu
+        bemoji
+        inputs.self.outputs.packages.x86_64-linux.bitwarden-rofi
+        inputs.self.outputs.packages.x86_64-linux.bose-connect-app-linux
+        blueberry
+        brightnessctl
+        dmenu
+        dunst
+        foot
+        grim
+        j4-dmenu-desktop
+        kitty
+        libnotify
+        maim
+        pinentry-bemenu
+        slurp
+        sway-audio-idle-inhibit
+        swayidle
+        swaylock
+        wl-clipboard
+        wtype
+        xdg-desktop-portal-wlr
+        xwaylandvideobridge
+      ];
+    };
+    light.enable = true;
+    waybar.enable = true;
   };
 
-  programs.waybar = {
-    enable = true;
-  };
   systemd.user.services.waybar = {
-    path = ["/run/current-system/sw"]; # /bin is added to this path automatically
+    path = [ "/run/current-system/sw" ]; # /bin is added to this path automatically
     serviceConfig = {
       Restart = "on-failure";
-      RestartSec = 5;
-      StartLimitIntervalSec = 60;
-      StartLimitBurst = 3;
+      #RestartSec = 5; #seconds
+      StartLimitIntervalSec = 0; #disable rate limiting
+      StartLimitBurst = 9;
     };
   };
-  programs.light.enable = true;
 }
