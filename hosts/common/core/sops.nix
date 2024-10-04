8b381cfc97904ea35346e0dbae8619e2a3447460
sigboe <sig.boe@gmail.com>
2024-10-04 12:00:53 +0200

    test

--- hosts/common/core/sops.nix
+++ hosts/common/core/sops.nix
@@ -18,7 +18,7 @@ in
         "/persist/etc/ssh/ssh_host_ed25519_key"
         "/etc/ssh/ssh_host_ed25519_key"
       ];
-      keyFile = "/persist/var/lib/sops-nix/key.txt";
+      keyFile = "/var/lib/sops-nix/key.txt";
       generateKey = true;
     };
 
