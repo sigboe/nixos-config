10e57ef004598f95adc819cc77cbc15ec7e54462
sigboe <sigboe@gmail.com>
2024-10-12 21:40:48 +0200

    Host: tala, Generation: 25, Version: "24.05.20241007.1bfbbbe", Kernel: "6.6.54"

--- hosts/tala/default.nix
+++ hosts/tala/default.nix
@@ -33,90 +33,93 @@
       # services
       ../common/optional/services/yubikey.nix
       ../common/optional/services/udisks.nix
+      ../common/optional/services/fwupd.nix
 
       #################### Users to Create ####################
       ../common/users/sigurdb
+    ];
+
+  # Enable bluetooth
+  boot = {
+    initrd.kernelModules = [ "btintel" ];
+    bootspec.enable = true;
+  };
+
+  # Enable hardware acceleration
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # webcam
+  hardware.ipu6 = {
+    enable = true;
+    platform = "ipu6ep"; #Alder Lake
+  };
+
+  # Enable networking
+  networking = {
+    hostName = "tala"; # Define your hostname.
+    networkmanager.enable = true;
+    #wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    firewall = {
+      #allowedTCPPorts = [22];
+      allowedUDPPorts = [
+        5182 # Wireguard
       ];
+      #enable = false;
+    };
+  };
 
-      # Enable bluetooth
-      boot = {
-      initrd.kernelModules = [ "btintel" ];
-      bootspec.enable = true;
+  security.polkit.enable = true;
+
+  services = {
+    # Ignore accidental powerkey press
+    logind = {
+      powerKey = "ignore";
+      powerKeyLongPress = "poweroff";
     };
 
-      # Enable hardware acceleration
-      hardware.opengl = {
-      enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
+    gnome.gnome-keyring.enable = true;
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  environment.shells = with pkgs; [ zsh ];
+
+  home-manager = {
+    useGlobalPkgs = true;
+  };
+
+  stylix = {
+    enable = true;
+    image = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/NixOS/nixos-artwork/refs/heads/master/wallpapers/nixos-wallpaper-catppuccin-mocha.png";
+      sha256 = "7e6285630da06006058cebf896bf089173ed65f135fbcf32290e2f8c471ac75b";
     };
-
-      # webcam
-      hardware.ipu6 = {
-      enable = true;
-      platform = "ipu6ep"; #Alder Lake
+    polarity = "dark";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    autoEnable = true;
+    targets = {
+      plymouth.enable = false;
     };
+  };
 
-      # Enable networking
-      networking = {
-      hostName = "tala"; # Define your hostname.
-      networkmanager.enable = true;
-      #wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-      firewall = {
-        #allowedTCPPorts = [22];
-        #allowedUDPPorts = [ ... ];
-        #enable = false;
-      };
-    };
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
-      security.polkit.enable = true;
-
-      services = {
-      # Ignore accidental powerkey press
-      logind = {
-        powerKey = "ignore";
-        powerKeyLongPress = "poweroff";
-      };
-
-      gnome.gnome-keyring.enable = true;
-    };
-
-      # Allow unfree packages
-      nixpkgs.config.allowUnfree = true;
-
-      environment.shells = with pkgs; [ zsh ];
-
-      home-manager = {
-      useGlobalPkgs = true;
-    };
-
-      stylix = {
-      enable = true;
-      image = pkgs.fetchurl {
-        url = "https://raw.githubusercontent.com/NixOS/nixos-artwork/refs/heads/master/wallpapers/nixos-wallpaper-catppuccin-mocha.png";
-        sha256 = "7e6285630da06006058cebf896bf089173ed65f135fbcf32290e2f8c471ac75b";
-      };
-      polarity = "dark";
-      base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-      autoEnable = true;
-      targets = {
-        plymouth.enable = false;
-      };
-    };
-
-      # Some programs need SUID wrappers, can be configured further or are
-      # started in user sessions.
-      # programs.mtr.enable = true;
-      # programs.gnupg.agent = {
-      #   enable = true;
-      #   enableSSHSupport = true;
-      # };
-
-      # This value determines the NixOS release from which the default
-      # settings for stateful data, like file locations and database versions
-      # on your system were taken. It‘s perfectly fine and recommended to leave
-      # this value at the release version of the first install of this system.
-      # Before changing this value read the documentation for this option
-      # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-      system.stateVersion = "24.05"; # Did you read the comment?
-      }
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+}
