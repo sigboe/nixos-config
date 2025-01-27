318565ee618938a8bc83737b6fa5c45546167398
sigboe <sigboe@gmail.com>
2025-01-27 18:59:42 +0100

    Host: tala, Generation: 80, Version: "24.11.20250119.107d5ef", Kernel: "6.6.72"
    M	hosts/common/optional/docker.nix

--- hosts/common/optional/docker.nix
+++ hosts/common/optional/docker.nix
@@ -14,11 +14,15 @@
     };
   };
 
+  # Enable the ability to run arm docker images
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
   # Useful other development tools
   environment.systemPackages = with pkgs; [
     dive # look into docker image layers
     podman-tui # status of containers in the terminal
     podman-compose # start group of containers for dev
     lazydocker # maybe make a wrapper that sets DOCKER_HOST="unix://$(podman machine inspect --format '{{.ConnectionInfo.PodmanSocket.Path}}')"
+    distrobox
   ];
 }
