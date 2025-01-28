7a78fb0ab8172693ce4900805b45e6162bd1ccec
sigboe <sigboe@gmail.com>
2025-01-28 14:21:20 +0100

    Host: tala, Generation: 82, Version: "24.11.20250119.107d5ef", Kernel: "6.6.72"
    M	hosts/common/optional/docker.nix

--- hosts/common/optional/docker.nix
+++ hosts/common/optional/docker.nix
@@ -1,13 +1,14 @@
 { pkgs, ... }:
 {
   # Enable common container config files in /etc/containers
-  virtualisation.containers.enable = true;
   virtualisation = {
+    containers.enable = true;
     podman = {
       enable = true;
 
       # Create a `docker` alias for podman, to use it as a drop-in replacement
       dockerCompat = true;
+      dockerSocket.enable = true;
 
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
@@ -16,6 +17,8 @@
 
   # Enable the ability to run arm docker images
   boot.binfmt.emulatedSystems = [ "aarch64-linux" "armv7l-linux" ];
+  # If you add to this list, or have used podman before setting this option, you might have to run
+  # sudo podman run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
   # Useful other development tools
   environment.systemPackages = with pkgs; [
