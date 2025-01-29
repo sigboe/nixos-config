2c510746e11f4263b037479b077cc921c5aaef82
sigboe <sigboe@gmail.com>
2025-01-29 19:42:37 +0100

    Host: tala, Generation: 84, Version: "24.11.20250128.2b4230b", Kernel: "6.6.72"
    M	flake.lock
    M	hosts/common/core/locale.nix

--- hosts/common/core/locale.nix
+++ hosts/common/core/locale.nix
@@ -1,9 +1,27 @@
-{
+{ config, ... }: {
   # Set your time zone.
   time.timeZone = "Europe/Oslo";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n = {
+    supportedLocales = [ "all" ];
+    defaultLocale = "no_NB.utf-8";
+    extraLocaleSettings = {
+      LANG = "en_US.utf8";
+      LC_CTYPE = config.i18n.defaultLocale;
+      LC_NUMERIC = config.i18n.defaultLocale;
+      LC_TIME = config.i18n.defaultLocale;
+      LC_COLLATE = config.i18n.defaultLocale;
+      LC_MONETARY = config.i18n.defaultLocale;
+      LC_MESSAGES = "en_US.utf8";
+      LC_PAPER = config.i18n.defaultLocale;
+      LC_NAME = config.i18n.defaultLocale;
+      LC_ADDRESS = config.i18n.defaultLocale;
+      LC_TELEPHONE = config.i18n.defaultLocale;
+      LC_MEASUREMENT = config.i18n.defaultLocale;
+      LC_IDENTIFICATION = config.i18n.defaultLocale;
+    };
+  };
 
   # Configure keymap in X11
   services.xserver.xkb = {
