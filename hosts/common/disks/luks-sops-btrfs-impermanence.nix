3490153523e476cece18592e19c128f2a8b53374
sigboe <sigboe@gmail.com>
2025-02-13 14:17:21 +0100

    1

--- hosts/common/disks/luks-sops-btrfs-impermanence.nix
+++ hosts/common/disks/luks-sops-btrfs-impermanence.nix
@@ -7,7 +7,7 @@
         type = "gpt";
         partitions = {
           ESP = {
-            size = "";
+            size = "2G";
             type = "EF00";
             content = {
               type = "filesystem";
