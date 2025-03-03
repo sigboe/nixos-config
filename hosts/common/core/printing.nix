16e07e2b34bae3d0e9ad5a86845b51c1b9bc6da9
sigboe <sigboe@gmail.com>
2025-03-03 12:32:06 +0100

    Host: tala, Generation: 113, Version: "24.11.20250219.36864ed", Kernel: "6.13.3"
    M	hosts/common/core/printing.nix

---
+++ hosts/common/core/printing.nix
@@ -0,0 +1,18 @@
+{ pkgs, lib, ... }: {
+  services = {
+    printing = {
+      enable = true;
+      drivers = [ pkgs.hplipWithPlugin ];
+    };
+    avahi = {
+      enable = lib.mkDefault true;
+      nssmdns4 = lib.mkDefault true;
+      openFirewall = lib.mkDefault true;
+    };
+  };
+  hardware.sane = {
+    enable = true;
+    extraBackends = [ pkgs.hplipWithPlugin ];
+  };
+  environment.systemPackages = [ pkgs.hplipWithPlugin ];
+}
