4b722b26502e4bf9f013f8cda0a50f3eedbe78eb
sigboe <sigboe@gmail.com>
2025-05-02 12:24:06 +0200

    Host: tala, Generation: 165, Version: "24.11.20250430.3e362ce", Kernel: "6.14.4"
    M	home/sigurdb/common/optional/sway/kanshi.nix

--- home/sigurdb/common/optional/sway/kanshi.nix
+++ home/sigurdb/common/optional/sway/kanshi.nix
@@ -18,7 +18,7 @@
             position = "3340,800";
           }
           {
-            criteria = ''output "GIGA-BYTE TECHNOLOGY CO., LTD. M34WQ FP0@:'';
+            criteria = ''GIGA-BYTE TECHNOLOGY CO., LTD. M34WQ FP0@:'';
             status = "enable";
             mode = "3440x1440@59.973Hz";
             position = "0,0";
