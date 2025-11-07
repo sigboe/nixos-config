ea208c5a8bc37d03b1e3a54817d65937cf8c879a
sigboe <sigboe@gmail.com>
2025-11-07 23:12:46 +0100

    Host: tala, Generation: 215, Version: "25.11.20251105.ae814fd", Kernel: "Unknown"
    M	hosts/common/optional/docker.nix

--- hosts/common/optional/docker.nix
+++ hosts/common/optional/docker.nix
@@ -3,29 +3,19 @@
   # Enable common container config files in /etc/containers
   virtualisation = {
     containers.enable = true;
-    podman = {
-      enable = true;
-
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
-      dockerSocket.enable = true;
-
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
-    };
+    docker.enable = true;
   };
 
   # Enable the ability to run arm docker images
   boot.binfmt.emulatedSystems = lib.optionals (pkgs.stdenv.hostPlatform == "x86_64-linux") [ "aarch64-linux" "armv7l-linux" ];
-  # If you add to this list, or have used podman before setting this option, you might have to run
-  # sudo podman run --rm --privileged multiarch/qemu-user-static --reset -p yes
+  # If you add to this list, or have used docker before setting this option, you might have to run
+  # sudo docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
   # Useful other development tools
   environment.systemPackages = with pkgs; [
     dive # look into docker image layers
-    podman-tui # status of containers in the terminal
-    podman-compose # start group of containers for dev
-    lazydocker # maybe make a wrapper that sets DOCKER_HOST="unix://$(podman machine inspect --format '{{.ConnectionInfo.PodmanSocket.Path}}')"
+    docker-compose # start group of containers for dev
+    lazydocker
     distrobox
   ];
 
