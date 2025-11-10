bb7a34545b227ce614e29c0eb7b68a4b5090df3f
sigboe <sigboe@gmail.com>
2025-11-10 12:00:13 +0100

    Host: tala, Generation: 217, Version: "25.11.20251108.b6a8526", Kernel: "6.17.7"

---
+++ hosts/common/optional/podman.nix
@@ -0,0 +1,34 @@
+{ config, lib, pkgs, ... }:
+{
+  # Enable common container config files in /etc/containers
+  virtualisation = {
+    containers.enable = true;
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+      dockerSocket.enable = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+
+  # Enable the ability to run arm docker images
+  boot.binfmt.emulatedSystems = lib.optionals (pkgs.stdenv.hostPlatform == "x86_64-linux") [ "aarch64-linux" "armv7l-linux" ];
+  # If you add to this list, or have used podman before setting this option, you might have to run
+  # sudo podman run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
+  # Useful other development tools
+  environment.systemPackages = with pkgs; [
+    dive # look into docker image layers
+    podman-tui # status of containers in the terminal
+    podman-compose # start group of containers for dev
+    lazydocker # maybe make a wrapper that sets DOCKER_HOST="unix://$(podman machine inspect --format '{{.ConnectionInfo.PodmanSocket.Path}}')"
+    distrobox
+  ];
+
+  users.users.${config.hostSpec.username}.extraGroups = [ "podman" ];
+
+}
