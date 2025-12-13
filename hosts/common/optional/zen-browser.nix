0137a9b252ca3e23704250c81ab8245b696509e7
sigboe <sigboe@gmail.com>
2025-12-13 11:21:32 +0100

    Host: tala, Generation: 221, Version: "26.05.20251211.2fbfb1d", Kernel: "Unknown"
    M	hosts/common/optional/zen-browser.nix

--- hosts/common/optional/zen-browser.nix
+++ hosts/common/optional/zen-browser.nix
@@ -1,6 +1,6 @@
 { isDefault ? false, config, lib, inputs, pkgs, ... }: {
   environment.systemPackages = [ inputs.zen-browser.packages."${pkgs.stdenv.hostPlatform.system}".twilight ];
-  home-manager.users.${config.hostSpec.username}.programs.zen-browser.preferences."media.webrtc.camera.allow-pipewire" = true;
+  home-manager.users.${config.hostSpec.username}.programs.zen-browser.policies.Preferences."media.webrtc.camera.allow-pipewire" = true;
 } // lib.optionalAttrs isDefault {
   home-manager.users.${config.hostSpec.username}.xdg.mimeApps =
     let
